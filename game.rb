19a20044f72d4fc8565d2ce4956f5caf0d10ac83
jgu <jeffwgu@gmail.com>
2015-04-07 23:26:08 -0600

    created game players and some basic player children

--- game.rb
+++ game.rb
@@ -1,14 +1,16 @@
 require "byebug"
 class Game
-  attr_accessor :payoffs
+  attr_accessor :payoffs, :p1, :p2
   ACTIONS_HASH = { "0": "Cooperate, Cooperate",
-                  "1": "Defect, Cooperate",
-                  "2": "Cooperate, Defect",
-                  "3": "Defect, Defect"
+                   "1": "Defect, Cooperate",
+                   "2": "Cooperate, Defect",
+                   "3": "Defect, Defect"
   }
 
   def initialize(options={})
     @payoffs = options[:payoffs]
+    @p1 = options[:p1]
+    @p2 = options[:p2]
   end
 
   def prisoners_dilemma
@@ -36,5 +38,83 @@ class Game
 
   def judge_payoff(payoff_1, payoff_2, player)
     payoff_1[player] > payoff_2[player] ? payoff_1 : payoff_2
+  end
+
+  def turn
+    p1_move = p1.move
+    p2_move = p2.move
+    play(p1_move, p2_move)
+  end
+
+  def play(p1_move, p2_move)
+    p1.move_memory_self << p1_move
+    p2.move_memory_self << p2_move
+    p1.move_memory_opponent << p2_move
+    p2.move_memory_opponent << p1_move
+    if p1_move == 1 && p2_move == 1
+      p1.payoff_memory << payoffs[0][0]
+      p2.payoff_memory << payoffs[0][1]
+      return payoffs[0]
+    elsif p1_move == 0 && p2_move == 0
+      p1.payoff_memory << payoffs[3][0]
+      p2.payoff_memory << payoffs[3][1]
+      return payoffs[3]
+    elsif p1_move == 0
+      p1.payoff_memory << payoffs[1][0]
+      p2.payoff_memory << payoffs[1][1]
+      return payoffs[1]
+    else
+      p1.payoff_memory << payoffs[2][0]
+      p2.payoff_memory << payoffs[2][1]
+      return payoffs[2]
+    end
+  end
+end
+
+class Player
+  #player class by default set up as tit for tat
+  attr_accessor :move_memory_self, :move_memory_opponent, :payoff_memory
+  def initialize
+    @move_memory_self = []
+    @move_memory_opponent = []
+    @payoff_memory = []
+  end
+
+  def payoff
+    payoff_memory.inject(0) { |sum, po| sum += po }
+  end
+
+  def cooperate
+    1
+  end
+
+  def defect
+    0
+  end
+
+  def first_move
+    move_memory_opponent.empty?
+  end
+
+  def move
+    return cooperate if first_move
+    return opponent_last_move
+  end
+
+  def opponent_last_move
+    move_memory_opponent[-1]
+  end
+end
+
+class MeanTitForTat < Player
+  def move
+    return defect if first_move
+    return opponent_last_move
+  end
+end
+
+class Mean < Player
+  def move
+    defect
   end
 end
--- game_test.rb
+++ game_test.rb
@@ -42,4 +42,83 @@ class GameTest < MiniTest::Test
     game.battle_of_the_sexes
     assert_equal ["Cooperate, Cooperate", "Defect, Defect"], game.nash_equilibrium
   end
+
+  def test_it_accepts_moves_and_provides_payoff
+    setup_game_and_players
+    assert_equal [2,2], @game.play(1,1)
+    assert_equal [3,0], @game.play(0,1)
+    assert_equal [0,3], @game.play(1,0)
+    assert_equal [1,1], @game.play(0,0)
+  end
+
+  def setup_game_and_players
+    #player class by default set up as tit for tat
+    @p1 = Player.new
+    @p2 = Player.new
+    @game = Game.new(p1: @p1, p2: @p2)
+    @game.prisoners_dilemma
+  end
+
+  def test_it_can_initialize_with_players
+    setup_game_and_players
+    assert_equal @p1, @game.p1
+    assert_equal @p2, @game.p2
+  end
+
+  def test_it_can_induce_a_turn
+    setup_game_and_players
+    assert_equal [2,2], @game.turn
+  end
+
+  def test_players_can_remember_game_outcomes
+    setup_game_and_players
+    @game.turn
+    assert_equal [1], @p1.move_memory_self
+    assert_equal [1], @p1.move_memory_opponent
+    assert_equal [2], @p1.payoff_memory
+  end
+
+  def test_tit_for_tat_results_in_cooperations
+    setup_game_and_players
+    3.times do
+      @game.turn
+    end
+    assert_equal 6, @p1.payoff
+    assert_equal 6, @p2.payoff
+  end
+
+  def test_mean_tit_for_tat_results_in_defect_winning
+    setup_game_and_players
+    p2 = MeanTitForTat.new
+    @game.p2 = p2
+    3.times do
+      @game.turn
+    end
+    assert_equal 3, @p1.payoff
+    assert_equal 6, p2.payoff
+  end
+
+  def test_mean_player_wins_v_tit_for_tat
+    setup_game_and_players
+    p2 = Mean.new
+    @game.p2 = p2
+    3.times do
+      @game.turn
+    end
+    assert_equal 2, @p1.payoff
+    assert_equal 5, p2.payoff
+  end
+
+  def test_mean_player_wins_v_tit_for_tat
+    setup_game_and_players
+    p1 = Mean.new
+    p2 = Mean.new
+    @game.p1 = p1
+    @game.p2 = p2
+    3.times do
+      @game.turn
+    end
+    assert_equal 3, p1.payoff
+    assert_equal 3, p2.payoff
+  end
 end
